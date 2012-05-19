8c47c6759a91cbc72633721a3edda162918d21f7
tcme <hi@this-connect.me>
2012-05-19 16:45:02 +0200

    moved tests

--- tests/runner.js
+++ tests/runner.js
@@ -3,8 +3,8 @@ var Testigo = require('./testigo/Source/testigo').Testigo,
 	Runner = new Testigo.Runners.Simple('node', Tests);
 
 // Import test cases
-require('./suites/test-pd').setup(Tests);
-require('./suites/test-planet').setup(Tests);
+require('./suites/test.pd').setup(Tests);
+require('./suites/test.planet').setup(Tests);
 
 // Run tests
 Runner.run();
