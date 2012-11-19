071a95279b4cdfcc81193a6ed786d8c06a1d7b13
tcme <hi@this-connect.me>
2012-11-19 08:56:26 +0000

    adds roundtrip benchmark

---
+++ bench/io.js
@@ -0,0 +1,20 @@
+var station = require('../station'),
+	path = require('path');
+
+var dir = path.dirname(path.relative(process.cwd(), process.argv[1]));
+
+station({
+	read: 8205, // [netsend]
+	write: 8206, // [netreceive]
+	flags: ['-noprefs', '-stderr', '-nogui',
+	'-open', dir + '/io.pd']
+})
+.on('connect', function(){
+	this.write('bang;\n');
+})
+.on('data', function(data){
+	//console.log('data', data);
+	this.write('bang;\n');
+})
+.create();
+
