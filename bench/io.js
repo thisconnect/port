5ffa4ff191cdcf751b983c07ea419c3f86fe5dfc
tcme <hi@this-connect.me>
2012-11-22 09:41:40 +0000

    log roundtrip

--- bench/io.js
+++ bench/io.js
@@ -6,14 +6,13 @@ var dir = path.dirname(path.relative(process.cwd(), process.argv[1]));
 station({
 	read: 8205, // [netsend]
 	write: 8206, // [netreceive]
-	flags: ['-noprefs', '-stderr', '-nogui',
-	'-open', dir + '/io.pd']
+	flags: ['-noprefs', '-stderr', '-nogui', '-open', dir + '/io.pd']
 })
 .on('connect', function(){
 	this.write('bang;\n');
 })
 .on('data', function(data){
-	//console.log('data', data);
+	console.log('data', data);
 	this.write('bang;\n');
 })
 .create();
