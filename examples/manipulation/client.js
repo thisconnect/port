973fa0423d148f3449c5473e33f9c25097652f1d
tcme <hi@this-connect.me>
2012-11-22 11:41:28 +0000

    makes manipulation touch friendly

--- examples/manipulation/client.js
+++ examples/manipulation/client.js
@@ -1,9 +1,11 @@
+
+
 var canvas = document.getElementById('osci'),
-	context = canvas.getContext('2d'),
-	blockWidth = canvas.width / 128;
+	context = canvas.getContext('2d');
 
 context.fillStyle = '#000';
 context.fillRect(0, 0, canvas.width, canvas.height);
+
 
 var socket = io.connect();
 
@@ -11,27 +13,29 @@ socket.on('connect', function(){
 	// console.log('connected');
 });
 
+var blockWidth = canvas.width / 128;
 
 function change(e){
 	if (down && socket.socket.connected){
-		var x = parseInt(e.clientX / blockWidth) - 1,
-			y = 1 - (e.clientY / canvas.height);
-		
-		console.log(x, y);
+		var clientX = e.clientX || e.changedTouches[0].pageX,
+			clientY = e.clientY || e.changedTouches[0].pageY,
+			x = parseInt(clientX / blockWidth) - 1,
+			y = 1 - (clientY / canvas.height);
 
 		socket.emit('data', x, y);
 		
 		context.fillStyle = '#000';
 		context.fillRect(x * blockWidth, 0, blockWidth, canvas.height);
-
 		context.fillStyle = '#fc0';
-		context.fillRect(x * blockWidth, e.clientY, blockWidth, canvas.height);
+		context.fillRect(x * blockWidth, clientY, blockWidth, canvas.height);
 	}
 }
 
+function stop(){ down = false; }
+
 var down = false;
 
-window.addEventListener('mouseup', function(){ down = false; });
+window.addEventListener('mouseup', stop);
 
 canvas.addEventListener('mousedown', function(e){
 	down = true;
@@ -39,4 +43,17 @@ canvas.addEventListener('mousedown', function(e){
 });
 
 canvas.addEventListener('mousemove', change);
+
+
+// touch stuff for your mobile
+
+canvas.addEventListener('touchstart', function(e){
+	e.preventDefault();
+	down = true;
+	change(e);
+}, false);
+canvas.addEventListener('touchmove', change, false);
+canvas.addEventListener('touchend', stop, false);
+canvas.addEventListener('touchcancel', stop, false); 
+
 
--- examples/manipulation/server.js
+++ examples/manipulation/server.js
@@ -7,13 +7,13 @@ var path = require('path'),
 	socketio = require('socket.io'),
 	station = require('../../station');
 
+//console.log('ip', require('os').networkInterfaces().en1[1].address);
 
 var dir = path.dirname(path.relative(process.cwd(), process.argv[1]));
 
 
 
-// create a http server to deliver the client side
-
+// http server
 var server = http.createServer(function(req, res){
 	if (req.url == '/'){
 		res.writeHead(200, {'Content-Type': 'text/html'});
@@ -27,7 +27,7 @@ var server = http.createServer(function(req, res){
 			'<script src="/client.js"></script>\n'
 		].join('\n'));
 
-	} else if(req.url == '/client.js'){
+	} else if (req.url.match(/^\/client\.js$/)){
 		res.writeHead(200, {'Content-Type': 'text/javascript'});
 		fs.createReadStream(dir + '/client.js')
 		.on('data', function(chunk){
@@ -41,7 +41,7 @@ var server = http.createServer(function(req, res){
 		res.writeHead(404, {'Content-Type': 'text/html'});
 		res.end('<h1>Not Found</h1>\n');
 	}
-}).listen(8118, '127.0.0.1');
+}).listen(8118);
 
 console.log('Server running at http://127.0.0.1:8118/');
 
@@ -52,19 +52,20 @@ console.log('Server running at http://127.0.0.1:8118/');
 var pd = station({
 	read: 8125, // [netsend]
 	write: 8126, // [netreceive]
-	flags: ['-noprefs', '-stderr', // '-nogui',
+	flags: ['-noprefs', // '-stderr', '-nogui',
 	'-open', dir + '/wavetable.pd']
 })
 .on('print', function(buffer){
 	//console.log('print', buffer.toString());
-});
+})
+.create();
 
 
 
 // create a socket.io instance to receive data from the client
 
 var io = socketio.listen(server, {
-	'transports': ['websocket'],
+	'transports': ['websocket', 'htmlfile', 'xhr-polling', 'jsonp-polling'],
 	'log level': 1,
 	'browser client minification': true,
 	'browser client gzip': true
@@ -73,15 +74,16 @@ var io = socketio.listen(server, {
 io.on('connection', function(socket){
 	//console.log('socket.io client connection');
 
-	pd.create();
-
-	socket.on('disconnect', function(){
-		pd.destroy();;
-	});
-
 	socket.on('data', function(x, y){
-		//console.log('mousedata', x, y);
-		pd.write('wave ' + x + ' ' + y + ';\n');
+		//console.log('position', x, y);
+		pd.write([x, y].join(' ') + ';\n');
 	});
 });
+
+
+
+
+
+
+
 
